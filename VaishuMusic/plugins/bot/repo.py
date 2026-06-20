3d7e5f8f77ebd1e1208f71656df8ae0eb83f66f3
Sɪɢᴍᴀ Tʜᴀᴋᴜʀ 👀 <reetsingh3666@gmail.com>
2026-06-21 03:22:20 +0530

    Update repo.py

--- VaishuMusic/plugins/bot/repo.py
+++ VaishuMusic/plugins/bot/repo.py
@@ -1,16 +1,4 @@
-# -----------------------------------------------
-# 🔸 ShashankMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
+
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -36,7 +24,7 @@ async def start(_, msg):
             ),
             InlineKeyboardButton(
                 "✦ ʀᴇᴘᴏ",
-                url="https://github.com/sigmathakur/Vaishu-Music"
+                url="https://github.com/sigmathakur/Vaishu-Music-V2"
             )
         ]
     ]
